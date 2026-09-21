9c877005defe7b32bd2aa2553027464c1bae5744
Christopsy <christopher.vincent.soriano@gmail.com>
2026-09-21 20:41:03 +0800

    New info for part B

--- queries/part_b_baseline .sql
+++ queries/part_b_baseline .sql
@@ -24,11 +24,8 @@ SELECT
     COUNT(p.payment_id) AS total_payments,
     ROUND(SUM(p.amount), 2) AS total_spent
 FROM customer AS c
-JOIN rental AS r
-    ON r.customer_id = c.customer_id
-JOIN payment AS p
-    ON p.rental_id = r.rental_id
-   AND p.customer_id = c.customer_id
+JOIN rental AS r ON r.customer_id = c.customer_id
+JOIN payment AS p ON p.rental_id = r.rental_id AND p.customer_id = c.customer_id
 GROUP BY c.customer_id, c.first_name, c.last_name
 ORDER BY total_spent DESC
 LIMIT 10;
@@ -41,28 +38,25 @@ SELECT
     COUNT(p.payment_id) AS total_payments,
     ROUND(SUM(p.amount), 2) AS total_spent
 FROM customer AS c
-JOIN rental AS r
-    ON r.customer_id = c.customer_id
-JOIN payment AS p
-    ON p.rental_id = r.rental_id
-   AND p.customer_id = c.customer_id
+JOIN rental AS r ON r.customer_id = c.customer_id
+JOIN payment AS p ON p.rental_id = r.rental_id AND p.customer_id = c.customer_id
 GROUP BY c.customer_id, c.first_name, c.last_name
 ORDER BY total_spent DESC
 LIMIT 10;
 
 -- Record after execution:
--- Execution time: 94.44 ms
--- Potential Bottleneck: Payment JOIN contributes much to the total costs, approximately 5558 of the total cost 7545.
+-- Execution time: 773.43ms
+-- Potential Bottleneck: Payment JOIN contributes much to the total costs, approximately 42365 of the total cost 55375.
 
 -- MAIN EXPLAIN OBSERVATIONS
 -- Type of Access for each table: customer = all are scanned, rental = index lookup using customer_id, payment = index lookup using both customer_id and rental_id
 -- Indexes used: No new indices used. rental and payment indices used.
--- Estimated rows processed: 407 GROUPED rows
--- Estimated cost of query and other relevant execution-plan: Nested Loops and Aggregations raise estimated costs to 7545.
+-- Estimated rows processed: 2355 GROUPED rows
+-- Estimated cost of query and other relevant execution-plan: Nested Loops and Aggregations raise estimated costs to 55375.
 
 -- =========================================================
--- QUERY 2: Payments above each customer's own average payment
--- Analytical question: Which individual rental payments were unusually high compared with that customer's normal spending?
+-- QUERY 2: Top 20 Customers and their 
+-- Analytical question: Who are the top 20 active customers with at least 10 rentals in terms of total amount spent, and what are their rental and payment activity metrics?
 -- Uses: customer + rental + payment
 -- Includes the required correlated subquery.
 -- =========================================================
@@ -76,21 +70,12 @@ SELECT
     ROUND(SUM(p.amount), 2) AS total_spent,
     ROUND(AVG(p.amount), 2) AS average_payment
 FROM customer AS c
-JOIN rental AS r
-    ON r.customer_id = c.customer_id
-JOIN payment AS p
-    ON p.rental_id = r.rental_id
-   AND p.customer_id = c.customer_id
+JOIN rental AS r ON r.customer_id = c.customer_id
+JOIN payment AS p ON p.rental_id = r.rental_id AND p.customer_id = c.customer_id
 WHERE c.active = 1
-GROUP BY
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    c.store_id
+GROUP BY c.customer_id, c.first_name, c.last_name, c.store_id
 HAVING COUNT(DISTINCT r.rental_id) >= 10
-ORDER BY
-    total_spent DESC,
-    total_rentals DESC
+ORDER BY total_spent DESC, total_rentals DESC
 LIMIT 20;
 
 EXPLAIN
@@ -103,32 +88,23 @@ SELECT
     ROUND(SUM(p.amount), 2) AS total_spent,
     ROUND(AVG(p.amount), 2) AS average_payment
 FROM customer AS c
-JOIN rental AS r
-    ON r.customer_id = c.customer_id
-JOIN payment AS p
-    ON p.rental_id = r.rental_id
-   AND p.customer_id = c.customer_id
+JOIN rental AS r ON r.customer_id = c.customer_id
+JOIN payment AS p ON p.rental_id = r.rental_id AND p.customer_id = c.customer_id
 WHERE c.active = 1
-GROUP BY
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    c.store_id
+GROUP BY c.customer_id, c.first_name, c.last_name, c.store_id
 HAVING COUNT(DISTINCT r.rental_id) >= 10
-ORDER BY
-    total_spent DESC,
-    total_rentals DESC
+ORDER BY total_spent DESC, total_rentals DESC
 LIMIT 20;
 
 -- Record after execution:
--- Execution time: 98.431 ms
+-- Execution time: 776.866ms
 -- Potential Bottleneck: Inner Loop JOIN for payments costs too much.
 
 -- MAIN EXPLAIN OBSERVATIONS
 -- Type of Access for each table: customer = all are scanned, filtered by active = 1, rental & payment = index lookup
 -- Indexes used: No new indices used. rental and payment indices used.
--- Estimated rows processed: 407 GROUPED rows
--- Estimated cost of query and other relevant execution-plan: Nested Loops and Aggregations raise estimated costs to 3833.
+-- Estimated rows processed: 2355 GROUPED rows
+-- Estimated cost of query and other relevant execution-plan: Nested Loops and Aggregations raise estimated costs to 27773.
 
 -- =========================================================
 -- QUERY 3: Monthly Rental and Revenue Trend by Store
@@ -144,17 +120,10 @@ SELECT
     ROUND(SUM(p.amount), 2) AS total_revenue,
     ROUND(AVG(p.amount), 2) AS average_payment
 FROM rental AS r
-JOIN customer AS c
-    ON c.customer_id = r.customer_id
-LEFT JOIN payment AS p
-    ON p.rental_id = r.rental_id
-   AND p.customer_id = r.customer_id
-GROUP BY
-    c.store_id,
-    DATE_FORMAT(r.rental_date, '%Y-%m')
-ORDER BY
-    c.store_id ASC,
-    rental_month ASC;
+JOIN customer AS c ON c.customer_id = r.customer_id
+LEFT JOIN payment AS p ON p.rental_id = r.rental_id AND p.customer_id = r.customer_id
+GROUP BY c.store_id, DATE_FORMAT(r.rental_date, '%Y-%m')
+ORDER BY c.store_id ASC, rental_month ASC;
 
 EXPLAIN
 SELECT
@@ -165,27 +134,20 @@ SELECT
     ROUND(SUM(p.amount), 2) AS total_revenue,
     ROUND(AVG(p.amount), 2) AS average_payment
 FROM rental AS r
-JOIN customer AS c
-    ON c.customer_id = r.customer_id
-LEFT JOIN payment AS p
-    ON p.rental_id = r.rental_id
-   AND p.customer_id = r.customer_id
-GROUP BY
-    c.store_id,
-    DATE_FORMAT(r.rental_date, '%Y-%m')
-ORDER BY
-    c.store_id ASC,
-    rental_month ASC;
+JOIN customer AS c ON c.customer_id = r.customer_id
+LEFT JOIN payment AS p ON p.rental_id = r.rental_id AND p.customer_id = r.customer_id
+GROUP BY c.store_id, DATE_FORMAT(r.rental_date, '%Y-%m')
+ORDER BY c.store_id ASC, rental_month ASC;
 
 -- Record after execution:
--- Execution time: 142.265 ms
+-- Execution time: 1.2913844585418701s
 -- Potential Bottleneck: LEFT JOIN preserves rentals even when no matching payments exist.
 
 -- MAIN EXPLAIN OBSERVATIONS
 -- Type of Access for each table: customers = Index scan; scans store_id's, rental = Index lookup; finds rentals for each customer, payment = Index lookup
 -- Indexes used: No new indices created. Indices for rental and payment used. Index for store_id used in customer.
--- Estimated rows processed: 15881
--- Estimated cost of query and other relevant execution-plan: Nested inner and left joins leads to 11178 cost.
+-- Estimated rows processed: 121044
+-- Estimated cost of query and other relevant execution-plan: Nested inner and left joins leads to 81588 cost.
 
 -- =========================================================
 -- QUERY 4: Difference in rental activity and revenue between active and inactive customers
@@ -217,11 +179,11 @@ GROUP BY c.active
 ORDER BY revenue DESC;
 
 -- Record after execution:
--- Execution time: 110.556 ms
+-- Execution time: 831.292ms
 -- Potential Bottleneck: The LEFT JOIN cost for rental and customer is too high.
 
 -- MAIN EXPLAIN OBSERVATIONS
 -- Type of Access for each table: customers = all, rental and payment = index lookup
 -- Indexes used: No new indices created. Indices for customer and rental used.
--- Estimated rows processed: 599
--- Estimated cost of query and other relevant execution-plan: Nested Loops and Aggregations raise estimated costs to 11021
+-- Estimated rows processed: 38.7
+-- Estimated cost of query and other relevant execution-plan: Nested Loops and Aggregations raise estimated costs to 81890.
